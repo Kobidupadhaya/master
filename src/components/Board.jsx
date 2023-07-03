282f5af7fcd9ddf53884d20d90108a1a0481a77b
kobid <upadhayakobid6@gmail.com>
2023-07-03 20:43:45 +0530

    introduced a player to the game. when clicking on a square, show either x or 0

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,48 +1,52 @@
 import { useState } from 'react';
 import Square from './square';
-
 const Board = () => {
-     const [Squares, setSquares] = useState(Array(9).fill(null));
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
 
-     const handleSquareclick = clickedposition=>{
-          setSquares((currentsquares)=>{
+  const handleSquareClick = clickedPosition => {
+    
+      if (squares[clickedPosition]) {
+        return;
+      }
+      setSquares(currentSquares =>{
+      return currentSquares.map((squareValue, position) => {
+        if (clickedPosition === position) {
+          return isXNext ? 'X' : 'O';
+        }
+        return squareValue;
+      });
+    });
+    setIsXNext(currentIsXNext => !currentIsXNext);
+  };
+  const renderSquare = position => {
+    return (
+      <Square
+        value={squares[position]}
+        onClick={() => handleSquareClick(position)}
+      />
+    );
+  };
 
-      return currentsquares.map((squareValue, position)=>{
-          if(clickedposition == position){
-               return 'x'
-          }
-          return squareValue;
-          });
-     });
-     };
-     const renderSquare =position =>{
-          return(
-               <Square
-                value={Squares[position]}
-                onClick={()=> handleSquareclick(position)}/>
-          );
-       };
-     return( 
-     <div className="board">
-
-     <div className="board-row"> 
-     {renderSquare(0)}
-     {renderSquare(1)}
-     {renderSquare(2)}
-     </div>
-
-     <div className="board-row" >
-     {renderSquare(3)}
-     {renderSquare(4)}
-     {renderSquare(5)}
-     </div>
-
-     <div className="board-row"> 
-     {renderSquare(6)}
-     {renderSquare(7)}
-     {renderSquare(8)}
-     </div>
-</div>
-     );
+  return (
+    <div className="board">
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
 };
+
 export default Board;
