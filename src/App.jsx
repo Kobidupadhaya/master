901e292a813c2d3c936e13e54de0be21d3efa33d
kobid <upadhayakobid6@gmail.com>
2023-07-03 10:44:38 +0530

    installed sass. created board and square components barebones

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './styles.scss';
+import Board from './components/Board';
+
+
+
+function App() {
+
+
+  return (
+    <div className='app'>
+      <Board  />
+    </div>
+  );
+}
+
+export default App;
